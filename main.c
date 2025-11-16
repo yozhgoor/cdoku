339fdb29e04970b37001466f4df5258927c458f1
Yohan Boogaert <yozhgoor@outlook.com>
2025-11-16 19:54:34 +0100

    Represent and print a board (#1)

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#define SIZE 9
+
+void printBoard(int board[SIZE][SIZE]) {
+    for (int row = 0; row < SIZE; row++) {
+        if (row % 3 == 0 && row != 0)
+            printf("---------------------\n");
+
+        for (int col = 0; col < SIZE; col++) {
+            if (col % 3 == 0 && col != 0)
+                printf("| ");
+
+            printf("%d ", board[row][col]);
+        }
+        printf("\n");
+    }
+}
+
 int main(void) {
-    printf("Hello, Sudoku!\n");
+    int board[SIZE][SIZE] = {0};
+
+    printBoard(board);
+
     return 0;
 }
