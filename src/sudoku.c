46a478ef023307849931d736c3c6d0b4b9cc4caa
Yohan Boogaert <yozhgoor@outlook.com>
2025-11-17 07:36:35 +0100

    Add tests for the puzzle solver (#5)

--- src/sudoku.c
+++ src/sudoku.c
@@ -2,61 +2,97 @@
 #include <stdio.h>
 
 int isValid(int board[SIZE][SIZE], int row, int col, int num) {
-    for (int x = 0; x < SIZE; x++)
-        if (board[row][x] == num) return 0;
+    for (int x = 0; x < SIZE; x++) {
+        if (board[row][x] == num) {
+            return FAILURE;
+        }
+    }
 
-    for (int y = 0; y < SIZE; y++)
-        if (board[y][col] == num) return 0;
+    for (int y = 0; y < SIZE; y++) {
+        if (board[y][col] == num) {
+            return FAILURE;
+        }
+    }
 
     int startRow = (row / 3) * 3;
     int startCol = (col / 3) * 3;
 
-    for (int y = 0; y < 3; y++)
-        for (int x = 0; x < 3; x++)
-            if (board[startRow + y][startCol + x] == num)
-                return 0;
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 3; x++) {
+            if (board[startRow + y][startCol + x] == num) {
+                return FAILURE;
+            }
+        }
+    }
 
-    return 1;
+    return SUCCESS;
 }
 
-int solve(int board[SIZE][SIZE]) {
-    int row = -1, col = -1;
-    int foundEmpty = 0;
+int isBoardValid(int board[SIZE][SIZE]) {
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            int num = board[row][col];
+            if (num != 0) {
+                board[row][col] = 0;
+                if (isValid(board, row, col, num) == FAILURE) {
+                    board[row][col] = num;
+                    return FAILURE;
+                }
+                board[row][col] = num;
+            }
+        }
+    }
+    return SUCCESS;
+}
 
+int findEmptyCell(int board[SIZE][SIZE], int *row, int *col) {
     for (int r = 0; r < SIZE; r++) {
         for (int c = 0; c < SIZE; c++) {
             if (board[r][c] == 0) {
-                row = r;
-                col = c;
-                foundEmpty = 1;
-                break;
+                *row = r;
+                *col = c;
+                return SUCCESS;
             }
         }
-        if (foundEmpty) break;
+    }
+    return FAILURE;
+}
+
+int solve(int board[SIZE][SIZE]) {
+    int row, col;
+
+    if (isBoardValid(board) == FAILURE) {
+        return FAILURE;
     }
 
-    if (!foundEmpty) return 1;
+    if (findEmptyCell(board, &row, &col) == FAILURE) {
+        return SUCCESS;
+    }
 
-    for (int num = 1; num <= SIZE; num++) {
-        if (isValid(board, row, col, num)) {
+    for (int num = 1; num <= 9; num ++) {
+        if (isValid(board, row, col, num) == SUCCESS) {
             board[row][col] = num;
 
-            if (solve(board)) {
-                return 1;
+            if (solve(board) == SUCCESS) {
+                return SUCCESS;
             }
 
             board[row][col] = 0;
         }
     }
 
-    return 0;
+    return FAILURE;
 }
 
 void printBoard(int board[SIZE][SIZE]) {
     for (int row = 0; row < SIZE; row++) {
-        if (row % 3 == 0 && row != 0) printf("---------------------\n");
+        if (row % 3 == 0 && row != 0) {
+            printf("---------------------\n");
+        }
         for (int col = 0; col < SIZE; col++) {
-            if (col % 3 == 0 && col != 0) printf("| ");
+            if (col % 3 == 0 && col != 0) {
+                printf("| ");
+            }
             printf("%d ", board[row][col]);
         }
         printf("\n");
